10a4320d55657ea5dd908694fd0fdc233b4f1ec0
Mykhaylo Myroshnychenko <mykhail.miroshnichenko@amadeus.com>
2019-07-09 09:43:08 +0300

    lesson 1

--- src/main/java/com/findme/models/Message.java
+++ src/main/java/com/findme/models/Message.java
@@ -1,7 +1,10 @@
 package com.findme.models;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "MESSAGE")
 public class Message {
     private Long id;
     private String text;
@@ -9,4 +12,75 @@ public class Message {
     private Date dateRead;
     private User userFrom;
     private User userTo;
+
+    @Id
+    @SequenceGenerator(name = "MESSAGE_SEQ",sequenceName = "MESSAGE_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MESSAGE_SEQ")
+    @Column(name = "ID")
+    public Long getId() {
+        return id;
+    }
+
+    @Column(name = "TEXT")
+    public String getText() {
+        return text;
+    }
+
+    @Column(name = "DATE_SENT")
+    public Date getDateSent() {
+        return dateSent;
+    }
+
+    @Column(name = "DATE_READ")
+    public Date getDateRead() {
+        return dateRead;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "USER_FROM_ID", nullable = false)
+    public User getUserFrom() {
+        return userFrom;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "USER_TO_ID", nullable = false)
+    public User getUserTo() {
+        return userTo;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setDateSent(Date dateSent) {
+        this.dateSent = dateSent;
+    }
+
+    public void setDateRead(Date dateRead) {
+        this.dateRead = dateRead;
+    }
+
+    public void setUserFrom(User userFrom) {
+        this.userFrom = userFrom;
+    }
+
+    public void setUserTo(User userTo) {
+        this.userTo = userTo;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", dateSent=" + dateSent +
+                ", dateRead=" + dateRead +
+                ", userFrom=" + userFrom.getId() +
+                ", userTo=" + userTo.getId() +
+                '}';
+    }
 }
--- src/main/java/com/findme/models/Post.java
+++ src/main/java/com/findme/models/Post.java
@@ -1,7 +1,10 @@
 package com.findme.models;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "POST")
 public class Post {
     private Long id;
     private String message;
@@ -13,4 +16,54 @@ public class Post {
 
     //TODO
     //comments
+
+    @Id
+    @SequenceGenerator(name = "POST_SEQ", sequenceName = "POST_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "POST_SEQ")
+    @Column(name = "ID")
+    public Long getId() {
+        return id;
+    }
+
+    @Column(name = "MESSAGE")
+    public String getMessage() {
+        return message;
+    }
+
+    @Column(name = "DATE_POSTED")
+    public Date getDatePosted() {
+        return datePosted;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "USER_POSTED_ID", nullable = false)
+    public User getUserPosted() {
+        return userPosted;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setDatePosted(Date datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    public void setUserPosted(User userPosted) {
+        this.userPosted = userPosted;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", message='" + message + '\'' +
+                ", datePosted=" + datePosted +
+                ", userPosted=" + userPosted.getId() +
+                '}';
+    }
 }
